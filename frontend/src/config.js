fdcc84da1df1a9db00298756aeb126f9a9689321
Ibrahim Abdulkareem <55307629+oris-96@users.noreply.github.com>
2025-09-29 10:53:20 -0400

    update the url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,6 +2,6 @@
 
 const config = {
   backendUrl:
-    'http://devops-challenge-alb-1494900872.us-east-2.elb.amazonaws.com/api',
+    'http://devops-challenge-alb-1494900872.us-east-2.elb.amazonaws.com/api/',
 };
 export default config;
